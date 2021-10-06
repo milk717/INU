2753decfd4f1f8d15403567e75b9c267aced9dc1
milk717 <57657868+milk717@users.noreply.github.com>
2021-10-06 18:24:56 +0900

    Add files via upload

---
+++ 2학년2학기/윈도우즈프로그래밍/ch5/Value_Type.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch5_Method
+{
+    class Value_Type
+    {
+        static void CallByValue(int x)
+        {
+            Console.WriteLine("함수내로 전달된 값 : {0}", x);
+            x = 1000;
+            Console.WriteLine("함수내에서 재 지정한 값 : {0}", x);
+        }
+        static void Main(string[] args)
+        {
+            int y = 50;
+            CallByValue(y);
+            Console.WriteLine("Call By Value : {0}",y);
+        }
+    }
+}
