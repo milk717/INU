560ce7a488fff6dc8d5595c4da0fbe6200bbf312
milk717 <57657868+milk717@users.noreply.github.com>
2021-10-18 20:13:33 +0900

    Add files via upload

---
+++ 2학년2학기/윈도우즈프로그래밍/ch4_배열/Collection_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace ch4_배열
+{
+    class Collection_A
+    {
+        static void Main(string[] args)
+        {
+            ArrayList AL = new ArrayList();
+            AL.Add(123);
+            AL.Add("hello");
+            AL.Add(DateTime.Now);
+            AL.Add(123.45);
+
+            for(int i = 0; i<AL.Count; i++)
+            {
+                Console.WriteLine(AL[i]);
+            }
+        }
+    }
+}
