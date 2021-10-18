32f6eac2e5a54efe2151bb3c89807f4900bcbb77
milk717 <57657868+milk717@users.noreply.github.com>
2021-10-18 20:15:44 +0900

    Add files via upload

---
+++ 2학년2학기/윈도우즈프로그래밍/ch6_Class/생성자.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch6_Class
+{
+    class sbs
+    {
+        private static string name = "홍서방";
+        private int age = 19;
+        public static void Print()
+        {
+            Console.WriteLine("정적 이름은: {0}",name);
+        }
+        public void Out()
+        {
+            Console.WriteLine("인스턴스 나이는 : {0}",age);
+        }
+        public sbs()
+        {
+            Console.WriteLine("Degault C#");
+        }
+    }
+    class 생성자
+    {
+        static void Main(string[] args)
+        {
+            sbs.Print();
+
+            sbs k1 = new sbs();
+            k1.Out();
+        }
+    }
+}
