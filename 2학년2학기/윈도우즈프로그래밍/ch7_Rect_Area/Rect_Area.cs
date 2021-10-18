49123a5aee54832c4ae3a8974efd29ad043ded38
milk717 <57657868+milk717@users.noreply.github.com>
2021-10-18 20:17:10 +0900

    Add files via upload

---
+++ 2학년2학기/윈도우즈프로그래밍/ch7_Rect_Area/Rect_Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Area_OP; // 추가
+class Rect_Area
+{
+    static void Main(string[] args)
+    {
+        Console.Write("사각형 가로 입력 : ");
+        int width = Convert.ToInt16(Console.ReadLine());
+        Console.Write("사각형 세로 입력 : ");
+        int height = Convert.ToInt16(Console.ReadLine());
+        Area_Calc s = new Area_Calc(width, height);
+        Print_out(s.Width, s.Height, s.Area, s.Length);
+    }
+    private static void Print_out(int w, int h, int sum, int len)
+    {
+        Console.WriteLine("==============================");
+        Console.WriteLine("가로\t세로\t면적\t둘레");
+        Console.WriteLine(w + "\t" + h + "\t" + sum + "\t" + len);
+        Console.WriteLine();
+    }
+}
+
+
