8d3b46fd1bf2c41d24a08f566b4dfee47ad87e43
milk717 <57657868+milk717@users.noreply.github.com>
2021-10-22 15:32:47 +0900

    Add files via upload

---
+++ 2학년2학기/C++/Ch1/1-2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout <<"컴퓨터공학과\n";
+    cout <<"21세\n";
+    cout <<"나사에서 일하기\n";
+    return 0;
+}
---
+++ 2학년2학기/C++/Ch1/1-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+	int sum = 0;
+	for (int i = 0; i <= 10; i++) {
+		sum += i;
+	}
+	std::cout << sum << "ÀÔ´Ï´Ù"<<std::endl;
+	return 0;
+}
