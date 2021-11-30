8371d96783d108d271425b7cf317f42452ccb6dc
milk717 <robolindasoo@gmail.com>
2021-11-30 20:23:51 +0900

    히히

--- 1학년1학기/프로그래밍 입문/과제/과제9_1번_카이사르 암호문 만들기.py
+++ 1학년1학기/프로그래밍 입문/과제/과제9_1번_카이사르 암호문 만들기.py
@@ -1,3 +1,4 @@
+#‘ ’, ‘A’, ‘B’, ‘C’, …, ‘X’, ‘Y’, ‘Z’, ‘a’, ‘b’, ‘c’, …, ‘x’, ‘y’, ‘z’
 def encipher(p, k):
     a = ord(p)
     if 64<a<91:
@@ -13,20 +14,20 @@ def encipher(p, k):
         t = t - 6
     return chr(t)
     
+while(True):
+    k = input('k = ')
+    if(k.isdigit()):
+        k = int(k)
+        break
+    else:
+        print("양의 정수만 입력해주세요!")
 
-k = int(input('k = '))
-p = 'Abc xyZ aBC XYz'
-print('평문 : ',p)
-C = ''
+p = input("p = ")
+print('\n평문 : ',p)
+ciphertext = ''
 
 for i in range(0, len(p)):
     c = encipher(p[i], k)
-    C = C + c
-print('암호문 : [',end='')
-print(C, end='')
-print(']')
-    
+    ciphertext = ciphertext + c
 
-
-
-    
+print("암호문 : [ %s ]"%ciphertext)
